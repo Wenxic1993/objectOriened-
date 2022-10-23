d1bf9eab95f285e60274ecc1460f24eb5fd5487b
Wenxic1993 <412664771@qq.com>
2022-10-23 19:31:45 +0100

    Java

---
+++ Car.java
@@ -0,0 +1,34 @@
+package statucexample;
+
+public class Car {
+	public static int currentSpeed=0;
+	public static int maxSpeed=0;
+	
+	public static void showCurrentSpeed(int speed) {
+		System.out.println("You current speed is" + speed);
+	}
+	
+	public static void speedUp(int increase) {
+		
+		currentSpeed +=increase;
+		if (currentSpeed > maxSpeed) {
+			showCurrentSpeed (currentSpeed);
+			System.out.println("Please slow down");
+		}
+		
+		else {
+			showCurrentSpeed (currentSpeed);
+		}
+	}
+		
+		public static void speedDown(int decrease) {
+			currentSpeed -=decrease;
+			showCurrentSpeed (currentSpeed);
+	} 
+	
+		public static void stop() {
+			currentSpeed=0;
+			showCurrentSpeed (currentSpeed); 
+			
+		}
+}
---
+++ Test.java
@@ -0,0 +1,14 @@
+package statucexample;
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Car.showCurrentSpeed(Car.currentSpeed);
+		Car.speedUp(60);
+		Car.speedUp(160);
+		Car.speedDown(100);
+		Car.stop();
+	}
+
+}
